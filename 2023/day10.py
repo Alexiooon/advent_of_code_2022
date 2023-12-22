fb37ffc250be662eaffeed4216620d0bc61baaa9
Alexander <59180358+Alexiooon@users.noreply.github.com>
2023-12-22 17:34:26 +0100

    Day 10 solution

--- 2023/day10.py
+++ 2023/day10.py
@@ -1,8 +1,6 @@
 """Day 10 challenges."""
 
 import numpy as np
-import sys
-sys.setrecursionlimit(5000)
 
 ROT_90 = np.array([[0, 1], [-1, 0]])
 
@@ -90,53 +88,29 @@ def get_next_position(grid: list[str], position: tuple[int, int], last_position:
     return pos_1 if pos_2 == last_position else pos_2
 
 
-def sweep_grid(grid: list[str], grid_scan: list, pipe_coordinates: list, start: tuple[int, int]):
-    """Recursively sweep and area for tiles which are not included in the main loop."""
-    y0, x0 = start
-    if _out_of_bounds(grid, start):
-        return
-    if start in pipe_coordinates:
-        grid_scan[y0][x0] = 1
-        return
-    if grid_scan[y0][x0] == 2:
-        return
-    grid_scan[y0][x0] = 2  # Inside the loop
-
-    # Scan adjacent tiles
-    for y, x in [(-1, -1), (-1, 1), (1, -1), (1, 1)]:
-        new_position = (y0+y, x0+x)
-        if new_position == start:
-            continue
-        if _out_of_bounds(grid, new_position):
-            continue
-        if grid_scan[y0+y][x0+x] == 0:  # Not scanned
-            sweep_grid(grid, grid_scan, pipe_coordinates, new_position)
-
-def right_hand_tile(current_position: tuple, next_position: tuple) -> tuple:
-    """Get the grid coordinates for the tile to the right i.e. 90 degree rotation."""
+def right_hand_corner(current_position: tuple, next_position: tuple, from_next: bool) -> tuple:
+    """Get the coordinates the "corner" to the right in front of the current position."""
     y0, x0 = current_position
     y1, x1 = next_position
-    vec_in = np.array([y1-y0, x1-x0])
-    dy, dx = np.dot(ROT_90, vec_in)
-    return y0+dy, x0+dx
+    vec_fwd = np.array([y1-y0, x1-x0])
+    vec_rhs = np.dot(ROT_90, vec_fwd)
+    y, x = (vec_fwd+vec_rhs) / 2
+    if from_next:
+        return next_position[0] + y, next_position[1] + x
+    return current_position[0] + y, current_position[1] + x
 
-
-def poly_area(xy):
-    """ 
-        Calculates polygon area.
-        x = xy[:,0], y = xy[:,1]
-    """
-    l = len(xy)
+def poly_area(coordinates: list[tuple[int, int]]):
+    """Calculates the area of a polygon."""
+    length = len(coordinates)
     s = 0.0
-    # Python arrys are zer0-based
-    for i in range(l):
-        j = (i+1)%l  # keep index in [0,l)
-        s += (xy[j,0] - xy[i,0])*(xy[j,1] + xy[i,1])
-    return -0.5*s
+    for i in range(length):
+        j = (i+1) % length  # Next index, looping around
+        s += (coordinates[j,0] - coordinates[i,0])*(coordinates[j,1] + coordinates[i,1])
+    return 0.5*s
 
 
 def main():
-    """Do not execute main functionality."""
+    """Solve todays challenges."""
     grid = read_input_data("./data/day_10_pipes.txt")
     start = find_starting_position(grid)
 
@@ -145,55 +119,39 @@ def main():
     i = 1  # Iteration variable, by finding the connections to "S" we've already moved 1 step
     last_pos_1 = start
     last_pos_2 = start
+    pipe_coordinates = [pos_1, pos_2]  # Coordinates of all pipes in the main loop, used in part 2
     while True:
         pos_1, last_pos_1 = get_next_position(grid, pos_1, last_pos_1), pos_1
         pos_2, last_pos_2 = get_next_position(grid, pos_2, last_pos_2), pos_2
-        i += 1
+        pipe_coordinates.append(pos_1)
+        pipe_coordinates.append(pos_2)
 
+        i += 1
         if pos_1 == pos_2:  # Paths connected
             break
     print(f"Paths connected after {i} iterations")
 
     # Part 2
-    # Create a list of all pipe coordinates
-    pos_1, _ = get_connections_in(grid, start)  # pylint: disable=unbalanced-tuple-unpacking
-    pipe_coordinates = [pos_1]  # Coordinates of all pipes in the main loop
+    # Find the most top-left pipe coordinate in the main loop (Row has priority over column)
+    start = sorted(pipe_coordinates)[0]
+    pos_1 = (start[0], start[1]+1)  # First tile is guaranteed "F", now we can choose the direction
     last_pos_1 = start
+
+    # We calculate the area inside a closed loop with Greens theorem. The only issue is that even
+    # the pipe tiles are included, and the enclosed area from a corner depends on whether its an
+    # inner corner (1/4th of a tile) or outside corner (3/4th). So, we loop through the pipe and
+    # get the coordinates to the inner side of the pipe for each tile.
+    polygon_coordinates = []
     while True:
+        polygon_coordinates.append(right_hand_corner(last_pos_1, pos_1, from_next=False))
+        polygon_coordinates.append(right_hand_corner(last_pos_1, pos_1, from_next=True))
         pos_1, last_pos_1 = get_next_position(grid, pos_1, last_pos_1), pos_1
-        pipe_coordinates.append(pos_1)
         if pos_1 == start:  # Full loop
             break
 
-    # Find the most top-left pipe coordinate in the main loop (Row has priority over column)
-    start = sorted(pipe_coordinates)[0]
-    grid_scan = np.zeros(shape=(len(grid[0]), len(grid[0])), dtype=int)  # Array of zeroes
-    pos_1 = (start[0], start[1]+1)
-    last_pos_1 = start
+    area = int(poly_area(np.array(polygon_coordinates)))  # Minus sign due to the direction we chose
+    print(f"The number of tiles enclosed by the loop is value: {area}")
 
-    # Move a full loop through the pipe. For each step, get the tile to the right (relative to
-    # travelling direction) and sweep for tiles not included in the main loop. All things to the
-    # right of the loop in the moving direction is either another pipe or a tile enclosed by the
-    # loop. (Since we always start of an "F" and move to the right)
-    # rht = right_hand_tile(last_pos_1, pos_1)
-    # sweep_grid(grid, grid_scan, pipe_coordinates, rht)
-
-    # while True:
-    #     pos_1, last_pos_1 = get_next_position(grid, pos_1, last_pos_1), pos_1
-    #     rht = right_hand_tile(last_pos_1, pos_1)
-    #     sweep_grid(grid, grid_scan, pipe_coordinates, rht)
-    #     if pos_1 == start:  # Full loop
-    #         break
-
-    print(f"A total of {np.sum(np.floor(grid_scan/2))} tiles are enclosed by the main loop")
-    print(f"A total of {len(pipe_coordinates)} tiles are in the main loop")
-    print(f"Calculated value: {poly_area(np.array(pipe_coordinates))}")
-    shape_1 = np.array([[0,0],[1,0],[1,1],[0,1]])
-    print(poly_area(shape_1))
 
 if __name__ == "__main__":
     main()
-
-# Correct value is between 441 < x < 453
-# \[ A=\sum_{i=1}^{N-1}I_{i+1,i}+I_{1,N} \] where e.g. \[ I_{2,1}=\intop_{x_{1}}^{x_{2}}ydx=\frac{1}{2}(y_{2}+y_{1})(x_{2}-x_{1}) \]
-#
